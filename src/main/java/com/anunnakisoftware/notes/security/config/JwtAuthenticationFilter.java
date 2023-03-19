2b95342317b2ea516fc0b70429defd8c1831b5f2
nenad <nenadzaric.shone@gmail.com>
2023-03-19 23:19:48 +0100

    Authentication filter added

---
+++ src/main/java/com/anunnakisoftware/notes/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.anunnakisoftware.notes.security.config;
+
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private String authHeader;
+    private String jwt;
+    public JwtAuthenticationFilter(String authHeader, String jwt) {
+        this.authHeader = authHeader;
+        this.jwt = jwt;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        authHeader  = request.getHeader("Authorization");
+
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+    }
+}
